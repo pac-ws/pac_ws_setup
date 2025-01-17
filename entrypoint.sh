a0a3a1d7918d104628b45c6a1009b09f821958e6
Saurav Agarwal <SauravAg@seas.upenn.edu>
2025-01-17 14:39:36 -0500

    comment launch

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,9 @@ export PYTHONPATH=/opt/venv/lib/python${PYTHON_VERSION}/site-packages:$PYTHONPAT
 #fi
 
 # if ROS_NAMESPACE is px4_$ROBOT_ID or starts with r#, where # is a number, then launch px4_origin.yaml
-if [[ "$ROS_NAMESPACE" == "px4_$ROBOT_ID" ]] || [[ "$ROS_NAMESPACE" =~ ^r[0-9]+$ ]]; then
-  ros2 launch /workspace/launch/starling_offboard.yaml
-fi
+# if [[ "$ROS_NAMESPACE" == "px4_$ROBOT_ID" ]] || [[ "$ROS_NAMESPACE" =~ ^r[0-9]+$ ]]; then
+#   ros2 launch /workspace/launch/starling_offboard.yaml
+# fi
 while [ 1 ]; do
   sleep 1
 done
