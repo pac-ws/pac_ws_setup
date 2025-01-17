5832382f97947136676fa4f9b7f1e40dd4ec688d
fred <fred.vatnsdal@gmail.com>
2025-01-17 12:47:16 -0500

    Comment gcs launch

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ echo "PYTHON_VERSION: $PYTHON_VERSION"
 source /workspace/install/setup.bash
 export PYTHONPATH=/opt/venv/lib/python${PYTHON_VERSION}/site-packages:$PYTHONPATH
 # if ROS_NAMESPACE is gcs, then launch gcs_origin.yaml
-if [ "$ROS_NAMESPACE" == "gcs" ]; then
-  ros2 launch /workspace/launch/gcs_origin.yaml
-fi
+#if [ "$ROS_NAMESPACE" == "gcs" ]; then
+#  ros2 launch /workspace/launch/gcs_origin.yaml
+#fi
 
 # if ROS_NAMESPACE is px4_$ROBOT_ID or starts with r#, where # is a number, then launch px4_origin.yaml
 if [[ "$ROS_NAMESPACE" == "px4_$ROBOT_ID" ]] || [[ "$ROS_NAMESPACE" =~ ^r[0-9]+$ ]]; then
