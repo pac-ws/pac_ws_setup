b5f4a161eecea017cbd830b81c30ca451219aa6c
Saurav Agarwal <SauravAg@seas.upenn.edu>
2025-01-18 15:24:00 -0500

    update for starling

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
+date
 echo "ROS_NAMESPACE: $ROS_NAMESPACE"
 echo "ROS_DOMAIN_ID: $ROS_DOMAIN_ID"
+echo "ROS_DISTRO: $ROS_DISTRO"
 echo "PYTHON_VERSION: $PYTHON_VERSION"
 
 if [[ "$ROS_NAMESPACE" =~ ^r[0-9]+$ ]]; then
+  source /opt/ros/$ROS_DISTRO/setup.bash
+  source /opt/ros/extra/install/local_setup.bash
+  export PYTHONPATH="/opt/venv/lib/python${PYTHON_VERSION}/site-packages:$PYTHONPATH"
   bash /workspace/pac_ws_setup/starling_build.bash
-fi
-
-if ! source /workspace/install/setup.bash; then
-  echo "Failed to source /workspace/install/setup.bash"
-  exit 1
-fi
-
-export PYTHONPATH="/opt/venv/lib/python${PYTHON_VERSION}/site-packages:$PYTHONPATH"
-
-# While check if mission_control_enable is set to true or not defined
-while [ -z "${mission_control_enable+x}" ] || [ "${mission_control_enable}" != "true" ]; do
-  mission_control_enable=$(ros2 param get --hide-type /mission_control enable 2>/dev/null)
-  sleep 1
-done
-
-# If ROS_NAMESPACE is px4_$ROBOT_ID or starts with r#, launch px4_origin.yaml
-if [[ "$ROS_NAMESPACE" == "px4_$ROBOT_ID" ]] || [[ "$ROS_NAMESPACE" =~ ^r[0-9]+$ ]]; then
+  source /workspace/install/local_setup.bash
+   
+  # While check if mission_control_enable is set to true or not defined
+  while [ -z "${mission_control_enable+x}" ] || [ "${mission_control_enable}" != "True" ]; do
+    mission_control_enable=$(ros2 param get --hide-type /mission_control enable 2>/dev/null)
+    sleep 2
+  done
+  echo "Mission Control Enabled, Launching Starling Offboard"
   ros2 launch /workspace/launch/starling_offboard.yaml
 fi
 
