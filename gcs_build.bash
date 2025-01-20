4e18a3aa9b672cf8ed2fef64255c781043806bdd
Saurav Agarwal <agr.saurav1@gmail.com>
2025-01-20 02:17:55 -0500

    fix

--- gcs_build.bash
+++ gcs_build.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-colcon build --packages-select coveragecontrol_sim async_pac_gnn_py async_pac_gnn_interfaces cc_rviz px4_homify starling_offboard_cpp starling_demos_cpp rviz_pac
+colcon build --packages-select coveragecontrol_sim async_pac_gnn_py async_pac_gnn_interfaces cc_rviz px4_homify starling_offboard_cpp starling_demos_cpp rviz_pac gcs
