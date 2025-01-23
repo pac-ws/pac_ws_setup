4f8c137dfc07ebd51f3972e6d3615b6a758ba4ab
fred <fred.vatnsdal@gmail.com>
2025-01-23 15:49:43 -0500

    Redirect bridge output to log file

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,10 @@ echo "ROS_DOMAIN_ID: $ROS_DOMAIN_ID"
 echo "ROS_DISTRO: $ROS_DISTRO"
 echo "PYTHON_VERSION: $PYTHON_VERSION"
 
+# Avoid cluttering the output with bridge messages but save the logs
+ZENOH_LOG_DIR=/workspace/log/zenoh
+mkdir -p $ZENOH_LOG_DIR
+
 while true; do
   # Extract the IP (IPv4) address for wlan0 from ifconfig output.
   # Depending on your OS, 'inet ' might appear as 'inet addr:' – adjust as needed.
@@ -30,8 +34,8 @@ if [[ "$ROS_NAMESPACE" =~ ^r[0-9]+$ ]]; then
   source /workspace/install/local_setup.bash
 
   # Starling bridge
-  ros2 run zenoh_vendor zenoh-bridge-ros2dds -c /workspace/src/zenoh_vendor/configs/zenoh_starling.json5
-   
+  ros2 run zenoh_vendor zenoh-bridge-ros2dds -c /workspace/src/zenoh_vendor/configs/zenoh_starling.json5 > $ZENOH_LOG_DIR/zenoh_bridge.log 2>&1 &
+
   # While check if mission_control_enable is set to true or not defined
   while [ -z "${mission_control_enable+x}" ] || [ "${mission_control_enable}" != "1" ]; do
     mission_control_enable=$(ros2 topic echo /mission_control --once 2>/dev/null | grep -A 1 "data:" | tail -n 1 | awk '{print $2}')
@@ -39,9 +43,10 @@ if [[ "$ROS_NAMESPACE" =~ ^r[0-9]+$ ]]; then
   done
   echo "Mission Control Enabled, Launching Starling Offboard"
   ros2 launch /workspace/launch/starling_offboard.yaml
+
 else
   # GCS bridge
-  ros2 run zenoh_vendor zenoh-bridge-ros2dds -c /workspace/src/zenoh_vendor/configs/zenoh_gcs.json5
+  ros2 run zenoh_vendor zenoh-bridge-ros2dds -c /workspace/src/zenoh_vendor/configs/zenoh_gcs.json5 > $ZENOH_LOG_DIR/zenoh_bridge.log 2>&1 &
 fi
 
 # Keep script alive
